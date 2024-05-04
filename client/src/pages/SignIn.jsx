887c256431d6175d8e88a3b8dc4cb3ac3eb2b0b1
ov150 <ov5010143@gmail.com>
2024-05-05 02:34:15 +0530

    login error

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,65 @@
-// import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
+
+  const handleChange =(event) =>{
+    setFormData({
+      ...formData, 
+      [event.target.id] : event.target.value,
+    })
+  }
+  console.log(formData);
+
+  const handleSubmit = async (event) =>{
+    event.preventDefault();
+    try {
+      const response = await fetch('/api/v1/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body:JSON.stringify(formData),
+      });
+      const data = response.json();
+      console.log(data);
+
+      if(response.ok){
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
+    <>
     <div>SignIn</div>
+    <form onSubmit={handleSubmit}>
+      <div>
+      <label htmlFor=""> email</label>
+      <input 
+        type="email" 
+        name="email" 
+        id="email" 
+        placeholder="enter your email"
+        onChange={handleChange}
+      />
+      </div>
+      <div>
+      <label htmlFor=""> password</label>
+      <input 
+        type="password" 
+        name="password" 
+        id="password" 
+        placeholder="enter your password"
+        onChange={handleChange}
+      />
+      </div>
+      <button type="submit"> Sign In</button>
+    </form>
+    </>
   )
 }
 
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 const signup = async (req, res, next) =>{
     try {
@@ -21,6 +22,42 @@ const signup = async (req, res, next) =>{
     }
 }
 
+const signin =  async (req, res, next) => {
+    try {
+        const { email, password} = req.body;
+        if(!username || !password || username === "" || password === ""){
+            next(errorHandler(400, 'all fields are required'));
+        }
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, 'user not found'));
+        }
+
+        return res.json({
+            message:"user login",
+            validUser
+        })
+        // if(password !== validUser.password){
+        //     return next(errorHandler(400, 'invalid password'))
+        // }
+
+        // const token = jwt.sign(
+        //     {id: validUser._id}, 
+        //     process.env.JWT_SECRET,
+        
+        // )
+
+        // const { password: pass, ...rest} = validUser._doc;
+
+        // res.status(200).cockie('access_token', token, {
+        //     httpOnly:true
+        // }).json(validUser)
+    } catch (error) {
+        next(error)
+    }
+}
+
 export {
-    signup
+    signup,
+    signin
 }
